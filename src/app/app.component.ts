b99f606d5e1b8d8df6b3452bd92f00cce67d4de2
Siddhant Prajapati <siddhantprajapati6377@gmail.com>
2024-05-20 21:46:44 +0530

    create barchart, linechart, piechart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BarChartComponent, LineChartComponent, PieChartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pie-chart/pie-chart.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto'
+
+@Component({
+  selector: 'app-pie-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './pie-chart.component.html',
+  styleUrl: './pie-chart.component.css'
+})
+export class PieChartComponent implements OnInit{
+
+  ngOnInit(): void {
+    this.createChart()
+  }
+  chart: any;
+
+  createChart(){
+
+    this.chart = new Chart("MyChart2", {
+      type: 'pie', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['Red', 'Pink','Green','Yellow','Orange','Blue', ],
+	       datasets: [{
+    label: 'My First Dataset',
+    data: [300, 240, 100, 432, 253, 34],
+    backgroundColor: [
+      'red',
+      'pink',
+      'green',
+			'yellow',
+      'orange',
+      'blue',			
+    ],
+    hoverOffset: 4
+  }],
+      },
+      options: {
+        aspectRatio:2.5
+      }
+
+    });
+  }
+}
